577fae0307991d7c1bd92c68a149827e32dd87f4
akaime11-beep <akaime11@gmail.com>
2026-05-07 00:20:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,4 @@
 const S_URL = 'https://wssmpxdsecqagfvetzqh.supabase.co';
 const S_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indzc21weGRzZWNxYWdmdmV0enFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNTEwMzMsImV4cCI6MjA5MzYyNzAzM30.1JTM3XGbkY6u2e4p-vLTZvhW-u36_4ryr5u_boI7Q80';
 const VK_APP_ID = '54580882';
+const VK_CLIENT_SECRET = 'lneSd3CbuNVTTmeDKdKA';
