56d530a1612e7e78c399d412ff2a09d14742055c
akaime11-beep <akaime11@gmail.com>
2026-05-06 15:22:56 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// Твои личные ключи Supabase
+const S_URL = 'https://wssmpxdsecqagfvetzqh.supabase.co';
+const S_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indzc21weGRzZWNxYWdmdmV0enFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNTEwMzMsImV4cCI6MjA5MzYyNzAzM30.1JTM3XGbkY6u2e4p-vLTZvhW-u36_4ryr5u_boI7Q80';
